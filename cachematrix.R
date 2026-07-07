822f561b39bbe9316f599dae41ae94371baf8a22
hugocavalheiro <hugo.cavalheiro@gmail.com>
2026-07-07 04:38:34 -0300

    Change of the code to support the answer of assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
+## Set the value of the matrix an reset the cache, inverse the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+  m <- NULL
+  
+  # Set the value of the matrix and reset the cache
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  
+  # Set the value of the inverse
+  setmean <- function(inverse) m <<- inverse
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+} 
 
 
 ## Write a short comment describing this function
-
+## Use of cached inverse matrix if exists
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmean()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmean(m)
+    m
 }
